3ad6f6906e530e3392a86e772a70f2b8eb440435
Collin <rocket3989@gmail.com>
2018-11-20 04:26:16 -0600

    added brushes

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 var r = [];
 var theta = [];
 var penDown = false;
+var cursor = false;
 var canvas = document.getElementById('drawing');
 var ctx = canvas.getContext('2d');
+var width = $('#line_width').val();
+var symmetry = $('#symmetry').val();
+var mirror = false;
+var brush = 'Normal';
+var start;
+// Changes to the form input trigger variable reloads
+$('#line_width').change(function (){
+	if ($('#line_width').val() <= 0)
+		$('#line_width').val(1);
+	width = $('#line_width').val();
+});
+$('#symmetry').change(function (){
+	if ($('#symmetry').val() <= 0){
+		alert("symmetry is typically constrained to counting numbers");
+		$('#symmetry').val(1);
+	}
+	symmetry = $('#symmetry').val();
+});
+$('input[type=radio][name=brush]').change(function(){
+	brush = this.value;
+	penDown = false;
+})
+$("#reset").click(function(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
 
 
 function transform(){
@@ -18,30 +45,82 @@ function getMousePos() {
       y: event.clientY - rect.top-canvas.height/2
     };
 }
+
+
+$('#drawing').mouseover(function(){
+	cursor = true;
+	$('.cursor').show();
+});
+$('#drawing').mouseout(function(){
+	cursor = false;
+	$('.cursor').hide();
+});
 $('body').mousedown(function(){
 	transform();
 	theta[0]=theta[1];
 	r[0]=r[1];
 	draw();
 	penDown = true;
+	start = {x: event.clientX, y:event.clientY-width/2};
+	
 });
 $('body').mouseup(function(){
-	penDown = false;});
-
-$('body').mousemove(function(){
-	if(penDown){
-		theta[0]=theta[1];
-		r[0]=r[1];
-		transform();
-		draw(false);
+	switch(brush){
+		case 'Straight':
+			transform();
+			draw();
+			$('.line').hide();
+		case 'Normal':
+		case 'Spray':
+			penDown = false;
+			break;
 	}
 });
-$("#reset").click(function(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+$('body').mousemove(function(){
+	switch(brush){
+		case 'Straight':
+			if(penDown)
+				lineDiv();
+			break;
+		case 'Normal':
+			if(penDown){
+				theta[0]=theta[1];
+				r[0]=r[1];
+				transform();
+				draw();
+			}
+			break;
+		case 'Spray':
+			if(penDown){
+				transform();
+				draw();
+			}
+			break;
+	}
+
+	$('.cursor').css({top: event.clientY-1-width/2, left:  event.clientX-1-width/2,width:width,height:width});
 });
+function lineDiv(){
+	
+	end = {x: event.clientX, y:event.clientY-width/2};	
+	let xdiff = start.x-end.x;
+	let ydiff = start.y-end.y;
+
+	let dist = Math.sqrt(xdiff*xdiff+ydiff*ydiff);
+	let slope = Math.atan2(ydiff,xdiff);
+	$('.line').css({
+		width: dist,
+		height: width,
+		top: (start.y+end.y)/2+1,
+		left: (start.x+end.x)/2 -dist/2 + 1,
+		transform: 'rotate(' + (slope*180)/Math.PI + 'deg)',
+		'background-color': rgbPick
+	});
+	$('.line').show();
+
+
+}
 function draw(){
-	let symmetry = $("#symmetry").val();
-	let width = $("#line_width").val();
 	ctx.lineWidth = width;
 	ctx.strokeStyle = rgbPick;
 	for(diff = 0; diff < symmetry; diff++){
@@ -57,5 +136,19 @@ function draw(){
 		ctx.arc(x1, y1, (width >= 2 ? (width/2) : 0), 0, 2 * Math.PI, false);
 		ctx.fillStyle = rgbPick;
 		ctx.fill();
+		if(mirror){
+			let x0 = canvas.width/2 + r[0]*Math.cos(-theta[0]+(diff*2*Math.PI)/symmetry);
+			let y0 = canvas.height/2 + r[0]*Math.sin(-theta[0]+(diff*2*Math.PI)/symmetry);
+			let x1 = canvas.width/2 + r[1]*Math.cos(-theta[1]+(diff*2*Math.PI)/symmetry);
+			let y1 = canvas.height/2 + r[1]*Math.sin(-theta[1]+(diff*2*Math.PI)/symmetry);
+			ctx.beginPath();
+			ctx.moveTo(x0,y0);
+			ctx.lineTo(x1,y1);
+			ctx.stroke();
+			ctx.beginPath();
+			ctx.arc(x1, y1, (width >= 2 ? (width/2) : 0), 0, 2 * Math.PI, false);
+			ctx.fillStyle = rgbPick;
+			ctx.fill();
+		}
 	}
 }
